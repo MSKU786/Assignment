99759be5403d0d702ee4587f1341a450a1aec15f
MANISH SINGH <manishsingh332000@gmail.com>
2021-06-09 19:15:16 +0530

    Navbar added

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Nav, Navbar, NavDropdown} from "react-bootstrap"
+
+function Topbar(props) {
+    
+    return (
+        <Navbar>
+        
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto ml-5">
+            <Nav.Link className="ml-4" > </Nav.Link>
+            <Navbar.Brand className="ml-4">Image</Navbar.Brand>
+          </Nav>
+          <Nav>
+            <Nav.Link className="mr-4" >  Search </Nav.Link>
+            <Nav.Link className="mr-4">Help</Nav.Link>
+            <Nav.Link className="mr-4">  Account </Nav.Link>
+            <Nav.Link className="mr-5"> </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+}
+
+export default Topbar;
